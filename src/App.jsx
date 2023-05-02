5489a75efe17ab6d7f54b72768784e1658ff413e
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-05-02 22:52:28 +0300

    fix: changed location of app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import Home from "../../pages/Home/Home";
-import Tweets from "../../pages/Tweets/Tweets";
-import { getUsers } from "../../redux/usersOperations";
-import SharedLayout from "../SharedLayout/SharedLayout";
+import Home from "./pages/Home/Home";
+import Tweets from "./pages/Tweets/Tweets";
+import { getUsers } from "./redux/usersOperations";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
 
 function App() {
   const dispatch = useDispatch();
